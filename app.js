6c5e8c3161a8214a15d468c4e149e3981a51218d
Micah Bai <Micah Bai>
2016-12-31 16:11:02 -0500

    Skype works and added random greetings

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var restify = require('restify');
 var builder = require('botbuilder');
+var myGreetings = ['Sup Dude', 'Hey Man', 'I can say more than Hello World now', 'Ayyyyy ma boi!!!', 'Hi', 'YO!', 'SUUUUUUHHHH'];
+var rand = myGreetings[Math.floor(Math.random() * myGreetings.length)];
 
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
-    console.log('%s listening to %s', server.name, server.url);
+    console.log('%s listening to %s', server.name, server.url)
     });
 
 //Create chat bot 
@@ -20,6 +22,6 @@ server.post('/api/messages', connector.listen());
 
 // Bot builder breaks fown conversations into components called dialogs. 
 bot.dialog('/', function (session) { //Bot builder tracks which dialog is currently active and will automatically route the incomeing message to the active dialog
-    session.send("Hello World"); // This is a single root '/' dialog that responds to any message with "Hello World"
+    session.send(rand); // This is a single root '/' dialog that responds to any message with "Hello World"
 });
 
